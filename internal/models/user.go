e2945ea5bae21da00f1aa191489154daa4c4aa46
Chris Casey <cdcasey@users.noreply.github.com>
2025-12-01 07:41:06 -0600

    Make timestamp time-based

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,11 +1,13 @@
 package models
 
+import "time"
+
 type User struct {
-	ID           string `json:"id"`
-	Email        string `json:"email"`
-	PasswordHash string `json:"-"` // - means never serialize password hash
-	Role         string `json:"role"`
-	CreatedAt    string `json:"created_at"`
+	ID           string    `json:"id"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"-"` // - means never serialize password hash
+	Role         string    `json:"role"`
+	CreatedAt    time.Time `json:"created_at"`
 }
 
 const (
