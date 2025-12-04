b7080e26bcd9a9ab0eb96afe6c71b18d57ffa8fe
Chris Casey <cdcasey@users.noreply.github.com>
2025-12-04 04:32:08 -0600

    Function for todo creation

--- internal/models/todo.go
+++ internal/models/todo.go
@@ -1,11 +1,27 @@
 package models
 
+import "time"
+
 type ToDo struct {
-	ID          string `json:"id"`
-	UserID      string `json:"user_id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Completed   string `json:"completed"`
-	CreatedAt   string `json:"created_at"`
-	UpdatedAt   string `json:"updated_at"`
+	ID          string    `json:"id"`
+	UserID      string    `json:"user_id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Completed   bool      `json:"completed"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
+
+func NewTodo(userID, title, description string) *ToDo {
+	now := time.Now()
+	return &ToDo{
+		ID:          NewID(),
+		UserID:      userID,
+		Title:       title,
+		Description: description,
+		Completed:   false,
+		CreatedAt:   now,
+		UpdatedAt:   now,
+	}
+}
+
