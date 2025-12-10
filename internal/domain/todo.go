2aeb7d10fb75c294b210dbd013e58fb9ae50fb2f
Chris Casey <cdcasey@users.noreply.github.com>
2025-12-09 20:59:27 -0600

    Todo domain

---
+++ internal/domain/todo.go
@@ -0,0 +1,28 @@
+package domain
+
+import (
+	"time"
+)
+
+type Todo struct {
+	ID          string    `json:"id"`
+	UserID      string    `json:"user_id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Completed   bool      `json:"completed"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+}
+
+func NewTodo(userID, title, description string) *Todo {
+	now := time.Now()
+	return &Todo{
+		ID:          NewId(),
+		UserID:      userID,
+		Title:       title,
+		Description: description,
+		Completed:   false,
+		CreatedAt:   now,
+		UpdatedAt:   now,
+	}
+}
