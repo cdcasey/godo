b9557603c89c86c20fd31c9ee0c6e6854c27143a
Chris Casey <cdcasey@users.noreply.github.com>
2025-12-09 20:52:41 -0600

    user domain

---
+++ internal/domain/user.go
@@ -0,0 +1,38 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type User struct {
+	ID           string    `json:"id"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"-"` // - means never serialize password hash
+	Role         string    `json:"role"`
+	CreatedAt    time.Time `json:"created_at"`
+}
+
+const (
+	RoleUser  = "user"
+	RoleAdmin = "admin"
+)
+
+func NewId() string {
+	return uuid.NewString()
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func (u *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.PasswordHash), []byte(password))
+	return err == nil
+}
