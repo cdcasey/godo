2f3bab023d99e1337b87eea1b909c1afa437251c
Chris Casey <cdcasey@users.noreply.github.com>
2025-12-11 23:59:25 -0600

    set up the cookie middleware

---
+++ internal/auth/cookie_middleware.go
@@ -0,0 +1,24 @@
+package auth
+
+import "net/http"
+
+func CookieMiddleware(secret string) func(http.HandlerFunc) http.Handler {
+	return func(next http.HandlerFunc) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			cookie, err := r.Cookie("auth_token")
+			if err != nil {
+				http.Redirect(w, r, "/login", http.StatusSeeOther)
+				return
+			}
+
+			claims, err := ValidateToken(cookie.Value, secret)
+			if err != nil {
+				http.Redirect(w, r, "/login", http.StatusSeeOther)
+				return
+			}
+
+			ctx := SetClaims(r.Context(), claims)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
